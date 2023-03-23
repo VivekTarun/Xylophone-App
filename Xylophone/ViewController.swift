636fcc07216429e92f9a6f2fa6616d0dd34c7bfb
vivektarun <vivektarun1234@gmail.com>
2023-03-23 16:13:48 +0530

    Sound file is connect to the code

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    
+    @IBAction func KeyPressed(_ sender: UIButton) {
+        playSound()
+    }
+    
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+        
+    }
+}
     
 
-}
 
